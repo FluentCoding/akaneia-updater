17a8748f6afabb89065fd54e36734eeec01a7ce9
ananas-dev <lucienfiorini@protonmail.com>
2021-01-28 00:06:40 +0100

    fix: :bug: fix user drag

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,13 @@ import Setup from './components/Setup'
 const useStyles = makeStyles(() => ({
   root: {
     display: 'flex',
+    userSelect: 'none',
+    userDrag: 'none'
   },
   logo: {
     height: "13rem",
     pointerEvents: "none",
     margin: "3rem",
-    userSelect: 'none',
-    userDrag: 'none'
   },
   updateButton: {
     marginBottom: "2rem",
@@ -38,7 +38,7 @@ export default function App() {
         alignItems="center"
         >
         <Grid item>
-          <img src={logo} alt="logo" className={classes.logo} draggable={false} />
+          <img src={logo} alt="logo" className={classes.logo} />
         </Grid>
         <Grid item>
           <Setup />
