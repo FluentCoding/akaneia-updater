f852009af4ca9417f9dd2a8768790e4400c3416a
ananas-dev <lucienfiorini@protonmail.com>
2021-02-01 16:03:46 +0100

    feat: :construction_worker: updater

--- src/components/Updater.jsx
+++ src/components/Updater.jsx
@@ -1,17 +1,25 @@
 import React, { useState, useEffect, useRef } from "react";
-import Container from '@material-ui/core/Container';
-import Paper from '@material-ui/core/Paper';
 import { Octokit } from '@octokit/core';
 import makeStyles from '@material-ui/styles/makeStyles';
-const Showdown = require('showdown');
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Typography from "@material-ui/core/Typography";
+import path from 'path';
 
 const useStyles = makeStyles((theme) => ({
+  instructions: {
+    margin: "0.5rem",
+    marginBottom: "2rem",
+    fontSize: "1.3rem"
+  }
 }));
 
 export default function ShowUpdate() {
+  const [version, setVersion] = useState(undefined);
+  const [assets, setAssets] = useState(undefined);
   const componentIsMounted = useRef(true);
-  const [releaseName, setReleaseName] = useState("");
-  const [releaseBody, setReleaseBody] = useState("");
 
   useEffect(() => {
     // each useEffect can return a cleanup function
@@ -23,40 +31,49 @@ export default function ShowUpdate() {
       async function fetchReleases() {
         try {
           const octokit = new Octokit();
-          const asyncResponse = await octokit.request('GET /repos/{owner}/{repo}/releases', {
-            owner: 'akaneia',
+          const asyncResponse = await octokit.request('GET /repos/{owner}/{repo}/releases/latest', {
+            owner: 'ananas-dev',
             repo: 'akaneia-build'
-          })
-          const data = asyncResponse.data[0];
-          const converter = new Showdown.Converter({headerLevelStart: 3});
-          converter.setFlavor('github')
-
-          console.log(data)
-
-          setReleaseName(data.name)
-          setReleaseBody(converter.makeHtml(data.body))
-
+          });
+          const data = asyncResponse.data;
           if (componentIsMounted.current) {
-            console.log(data);
+            setVersion(data.tag_name);
+            setAssets(data.assets);
+            console.log(data.assets[0])
           }
         } catch (err) {
           console.error(err);
         }
       }
     fetchReleases();
-  });
+  }, []);
 
 
   const classes = useStyles();
 
   return (
-    <Container>
-      <Paper className={classes.updateLogs}>
-        <h2>
-          {releaseName}
-        </h2>
-        <p dangerouslySetInnerHTML={{__html: releaseBody}} />
-      </Paper>
-    </Container>
+    <Box>
+      {assets && (
+        <Box>
+          <Typography className={classes.instructions}>
+            Choose the version you want to use
+          </Typography>
+          <ButtonGroup
+            color="primary"
+            orientation="vertical"
+            variant="contained"
+            aria-label="contained primary button group">
+              {assets.map(asset => (
+                <Button>
+                  {path.basename(asset.name, '.xdelta')}
+                </Button>
+              )
+            )
+          }
+          </ButtonGroup>
+        </Box>
+      )
+    }
+    </Box>
   );
 }
--- src/util/packageManager.js
+++ src/util/packageManager.js
@@ -1,7 +1,13 @@
-const config = require('./config');
-
 export default class packageManager {
-    constructor() {
-        this.config = config.get('githubRemoteUrl')
-    }
+  constructor() {
+    this.remote = "https://api.github.com/repos/akaneia/akaneia-build/releases"
+    this.version = "0.51"
+  }
+
+  async getReleaseInfo() {
+    const headers = new Headers({
+      "Accept": "application/vnd.github.v3+json"
+    })
+    return fetch(this.remote, headers).then(response => response);
+  }
 }
