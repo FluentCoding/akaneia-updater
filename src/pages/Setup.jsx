52d766e0c75828d064d44cfcfe23fa06b9e82b74
FluentCoding <zSuckyLP@gmail.com>
2021-02-05 21:24:06 +0100

    newer js feature

--- src/pages/Setup.jsx
+++ src/pages/Setup.jsx
@@ -109,7 +109,7 @@ const StepContent = ({ stepIndex }) => {
           accept=".iso"
           placeholder="Select an unmodified SSBM iso"
           key="0"
-          file={isoFile ? isoFile : store.get("vanillaIsoPath")}
+          file={isoFile ?? store.get("vanillaIsoPath")}
           setFile={setIsoFile}
         />
       );
